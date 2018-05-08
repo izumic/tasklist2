3b9e94a12a3cef6a7a7e2ebb038a2d10442be589
hiroko.tanaka <xxx@tmail.com>
2018-05-08 03:39:16 +0000

    user login_2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   
   def index
-    @user = User.all.page(params[:page])
+    @users = User.all.page(params[:page])
   end
 
   def show
